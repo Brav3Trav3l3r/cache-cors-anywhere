aefcad2b04499d2a3900a06f58a0e055391fd961
BraveTraveller <yashgajbhiye10@gmail.com>
2023-03-28 08:42:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const corsAnywhere = require('cors-anywhere');
 const express = require('express');
 const apicache = require('apicache');
 const expressHttpProxy = require('express-http-proxy');
-const CORS_PROXY_PORT = 5000;
+const CORS_PROXY_PORT = 5003;
 // Create CORS Anywhere server
 corsAnywhere.createServer({}).listen(CORS_PROXY_PORT, () => {
   console.log(
